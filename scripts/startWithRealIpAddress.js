852814fea9356c83dd42da7db3d5406e57ef8b3c
Craig Meinschein <cmeinsch@gmail.com>
2017-07-02 13:09:59 -0700

    Finish script to launch with real ip

--- scripts/startWithRealIpAddress.js
+++ scripts/startWithRealIpAddress.js
@@ -1,12 +1,26 @@
+// Figure out the IP address associated with my WiFi adapter because
+// otherwise the packager will pick my Docker NAT adapter's address.
 const os = require('os')
 const filter = require('lodash/filter')
 const child_process = require('child_process')
 
-const wifi = filter(os.networkInterfaces()['Wi-Fi'],
-    (net) => net.family === 'IPv4')
-const address = wifi[0].address
-const options = { env: process.env }
-options.env['REACT_NATIVE_PACKAGER_HOSTNAME'] = address
+const getWiFiAdapterAddress = () => {
+    const wifi = filter(os.networkInterfaces()['Wi-Fi'],
+        (net) => net.family === 'IPv4')
+    return wifi[0].address
+}
 
-// TODO link up stdout/stderr
-child_process.execFileSync('npm.cmd', ['start'], options)
+const options = { env: process.env }
+options.env['REACT_NATIVE_PACKAGER_HOSTNAME'] = getWiFiAdapterAddress()
+
+const p = child_process.spawn('npm.cmd', ['start'], options)
+
+p.stdout.on('data', (data) => {
+    console.log(data.toString())
+})
+p.stderr.on('data', (data) => {
+    console.log(data.toString())
+})
+p.on('exit', (code) => {
+    console.log('Child process exited with code ' + code.toString())
+})
